2ecb13be7681a73fe6ee31eecd0e39586d034eb8
mdr-manish <151599906+mdr-manish@users.noreply.github.com>
2024-01-02 17:58:35 +0900

    game create

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet,View } from 'react-native';
+import Game from './src/Game';
+import { LinearGradient } from "expo-linear-gradient"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#bde0fe", "#cdb4db"]} style={styles.container}>
+        <View style={styles.overlay}>
+          <Game />
+        </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container : {
+      flex: 1
   },
-});
+overlay: {
+  flex: 1,
+  backgroundColor: "rgba(255,255, 255, 0.1)",
+  justifyContent: "center",
+  alignItems: "center"
+}
+})
